897b37533f04338a56e429483935e62e8b102de2
Vlad <srvladyk555@gmail.com>
2020-01-27 17:54:09 +0200

    added thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {createStore, Store} from "redux";
+import {applyMiddleware, createStore, Store} from "redux";
 import rootReducer from "./redux/reducers";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk'
 
 import './App.scss';
 
-const store: Store = createStore(rootReducer);
+const store: Store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App: React.FC = () => {
   return (
